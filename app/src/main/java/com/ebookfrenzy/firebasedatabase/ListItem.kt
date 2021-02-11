da014fba0ab0c69fedd9c6b719d0ed979735418a
bwawczak80 <bwawczak@students.mchenry.edu>
2021-02-10 21:15:43 -0600

    Reading from Firebase

--- app/src/main/java/com/ebookfrenzy/firebasedatabase/ListItem.kt
+++ app/src/main/java/com/ebookfrenzy/firebasedatabase/ListItem.kt
@@ -1,3 +1,18 @@
 package com.ebookfrenzy.firebasedatabase
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.IgnoreExtraProperties
 
-data class ListItem (val nameTxt: String = "x", val speciesTxt: String? = "")
+@IgnoreExtraProperties
+data class ListItem (
+        val nameTxt: String? = null,
+        val speciesTxt: String? = null
+) {
+
+    @Exclude
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+                "nameTxt" to nameTxt,
+                "speciesTxt" to speciesTxt
+        )
+    }
+}
